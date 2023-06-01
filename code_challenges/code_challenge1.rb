0fa79b1d92297eb74252caf2284bcc5a80f54d5f
Antoine Aube <aaube3@gmail.com>
2023-06-01 12:45:07 -0600

    Add answers to challenge one: coding challenges

---
+++ code_challenges/code_challenge1.rb
@@ -0,0 +1,13 @@
+#Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+favorite_number = 2
+
+if favorite_number % 2 == 0
+    puts "even number"
+else
+    puts "odd number"
+end 
+
+
+
+
